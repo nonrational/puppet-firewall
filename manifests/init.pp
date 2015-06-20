9d7be4192616c9af80eca3c4f094eab59785d570
Alan Norton <me@alannorton.com>
2015-06-20 16:10:04 -0400

    don't go chasing firewalls

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,32 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Configure pf
+#
+# Examples
+#   include firewall
+class firewall {
+
+  $servicename  = "dev.pf"
+  $configdir  = "${boxen::config::configdir}"
+
+  service { $servicename:
+    ensure  => running
+  }
+
+  file { '/Library/LaunchDaemons/dev.pf.plist':
+    content => template('firewall/dev.pf.plist.erb'),
+    group   => 'wheel',
+    owner   => 'root',
+    notify  => Service[$servicename]
+  }
+
+  file { "${configdir}/pf.conf":
+    content => template('firewall/pf.conf.erb'),
+    notify  => Service[$servicename],
+    require => File[$configdir]
+  }
+
+  file { "${configdir}/pf.rules":
+    content => template('firewall/pf.rules.erb'),
+    notify  => Service[$servicename],
+    require => File[$configdir],
+  }
 }
