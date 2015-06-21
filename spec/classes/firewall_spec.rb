5ee811c38cf44824120cdb4802c433dc0851916f
Alan Norton <me@alannorton.com>
2015-06-21 13:35:22 -0400

    test cleanup

--- spec/classes/firewall_spec.rb
+++ spec/classes/firewall_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe 'firewall' do
-  let(:title) { 'nginx' }
-
   it do
     should contain_class('firewall')
+
     should contain_file('/Library/LaunchDaemons/dev.pf.plist')
-    # should contain_file("${boxen::config::configdir}/pf.conf")
-    # should contain_file("${boxen::config::configdir}/pf.rules")
+
+    should contain_file("${boxen::config::configdir}/pf.conf")
+    should contain_file("${boxen::config::configdir}/pf.rules")
   end
 end
